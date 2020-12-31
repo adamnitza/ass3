22ae8b87dde08488e7aed6e0f74c999f603f9a2a
Meitar <meitargo@post.bgu.ac.il>
2020-12-31 13:49:49 +0200

    main changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 
-int main() {
-    //hellow
 
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main() {
+    public void shortToBytes(short num, char* bytesArr)
+    {
+        bytesArr[0] = ((num >> 8) & 0xFF);
+        bytesArr[1] = (num & 0xFF);
+    }
+    //hellow
+    short cpCode = 4;
+    if(argc<3){
+        std::cerr << "Usage:" <<argv[0] <<"host port"<<std::endl;
+        reutrn -1;
+    }
+
+    std::string host = argv[1];
+    short port = atoi(argv[2]);
+
+    ConnectionHandler connectionHandler(host,port);
+    if(!connectionHandler.connect()){
+    std::cerr<< "Cannot connect to" << host<< ":" << port << std::endl;
+    retirn 1;
+    }
+    else{
+        shortToBytes(short);
+        connectionHandler.sendBytes();
+    }
+
+
 }
